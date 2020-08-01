1d3a77dc03322f65c4aec40b3552df8a700b815b
Gustavo Cruz <gustavo.cruz@infobase.com.br>
2020-08-01 11:50:44 -0300

    Classes de paginação

--- Quiron.LojaVirtual.UnitTest/UnitTest1.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Quiron.LojaVirtual.UnitTest
@@ -7,8 +8,28 @@ namespace Quiron.LojaVirtual.UnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Take()
         {
+            // O operadpr Take é usado para selecionar os primeiros n objetos de uma coleção.
+
+            int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var resultado = from num in numeros.Take(5) select num;
+            int[] teste = { 5, 4, 1, 3, 9 };
+
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
         }
+
+        [TestMethod]
+        public void Skip()
+        {
+            // O operadpr Skip igora o(s) primeiro(s) n objetos de uma coleção.
+
+            int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var resultado = from num in numeros.Take(5).Skip(2) select num;
+            int[] teste = { 1, 3, 9 };
+
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
+        }
+
     }
 }
