22a6b4a7991bb30f6864dfe576fa24c1ddf9f623
Gustavo Cruz <gustavo.cruz@infobase.com.br>
2020-09-17 10:05:35 -0300

    Módulo administrativo

---
+++ Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -0,0 +1,48 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidade;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+
+        // GET: Administrativo/Produto
+        public ActionResult Index()
+        {
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos;
+
+            return View(produtos);
+        }
+
+        public ViewResult Alterar(int produtoId)
+        {
+            _repositorio = new ProdutosRepositorio();
+            Produto produto = _repositorio.Produtos
+                .FirstOrDefault(p => p.ProdutoId == produtoId);
+            return View(produto);
+        }
+
+        [HttpPost]
+        public ActionResult Alterar(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                _repositorio = new ProdutosRepositorio();
+                _repositorio.Salvar(produto);
+
+                TempData["mensagem"] = string.Format("{0} foi salvo com sucesso", produto.Nome);
+
+                return RedirectToAction("Index");
+
+            }
+            return View(produto);
+        }
+    }
+}
